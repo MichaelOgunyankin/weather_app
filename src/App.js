4531a4ffb3e9183d9ae1583bb4c2275b4c8d4367
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-04-08 17:03:43 +0100

    Full functionality

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import { getFormattedWeatherData } from './weatherService';
 
 function App() {
 
+  const [city, setCity] = useState('London')
   const [weather, setWeather] = useState(null)
   const [units, setUnits] = useState('metric')
+  const [bg, setBg] = useState(hotBg)
 
   useEffect(() => {
     const fetchWeatherData = async () => {
 
-      const data = await getFormattedWeatherData('london', units)
+      const data = await getFormattedWeatherData(city, units)
       setWeather(data)
+
+      const threshold = units === "metric" ? 20 : 60;
+      if (data.temp <= threshold) setBg(coldBg);
+      else setBg(hotBg);
     }
     fetchWeatherData()
-  }, [units])
+  }, [units, city])
 
   const handleUnitsClick = (e) => {
     const button = e.currentTarget;
@@ -27,16 +33,35 @@ function App() {
     setUnits(isCelsius ? 'metric' : 'imperial');
   }
 
+  // const enterKeyPressed = (e) => {
+  //   if (e.KeyCode === 13) {
+  //     setCity(e.currentTarget.value)
+  //     e.currentTarget.blur()
+  //   }
+  // }
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
+
   return (
-    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+    <div className="app" style={{ backgroundImage: `url(${bg})` }}>
       <div className='overlay'>
         {
           weather && (
 
             <div className='container'>
-              <div className='section section__inputs'>
-                <input type='text' name='city' placeholder='Enter City...' />
+              {/* <div className='section section__inputs'>
+                <input onKeyDown={enterKeyPressed} type='text' name='city' placeholder='Enter City...' />
+
                 <button onClick={(e) => handleUnitsClick(e)} >°F</button>
+              </div> */}
+
+              <div className="section section__inputs">
+                <input onKeyDown={enterKeyPressed} type="text" name="city" placeholder="Enter City..." />
+                <button onClick={(e) => handleUnitsClick(e)}>°F</button>
               </div>
 
               <div className='section section__temperature'>
