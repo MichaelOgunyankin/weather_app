feea3ec29dc0b778b50f258eefbb6c382eb05a52
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-04-08 16:21:39 +0100

    Units conversion button functionality completed

--- src/App.js
+++ src/App.js
@@ -11,40 +11,49 @@ function App() {
 
   useEffect(() => {
     const fetchWeatherData = async () => {
-      
+
       const data = await getFormattedWeatherData('london', units)
-      setWeather( data )
+      setWeather(data)
     }
     fetchWeatherData()
-  }, [])
+  }, [units])
+
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1)
+
+    const isCelsius = currentUnit === 'C';
+    button.innerText = isCelsius ? '°F' : '°C'
+    setUnits(isCelsius ? 'metric' : 'imperial');
+  }
 
   return (
-    <div className="app" style={{backgroundImage: `url(${coldBg})`}}>
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
       <div className='overlay'>
         {
           weather && (
 
             <div className='container'>
-            <div className='section section__inputs'>
-              <input type='text' name='city' placeholder='Enter City...'/>
-              <button>°F</button>
-            </div>
-  
-            <div className='section section__temperature'>
-              <div className='description'>
-                <h3>{`${weather.name}, ${weather.country}`}</h3>
-                <img src={weather.iconURL} alt='Weather icon'/>
-                <h3>{weather.description}</h3>
+              <div className='section section__inputs'>
+                <input type='text' name='city' placeholder='Enter City...' />
+                <button onClick={(e) => handleUnitsClick(e)} >°F</button>
               </div>
-  
-              <div className='temperature'>
-                <h1>{`${weather.temp.toFixed()} °${units === 'metric' ? 'C' : 'F' }`}</h1>
+
+              <div className='section section__temperature'>
+                <div className='description'>
+                  <h3>{`${weather.name}, ${weather.country}`}</h3>
+                  <img src={weather.iconURL} alt='Weather icon' />
+                  <h3>{weather.description}</h3>
+                </div>
+
+                <div className='temperature'>
+                  <h1>{`${weather.temp.toFixed()} °${units === 'metric' ? 'C' : 'F'}`}</h1>
+                </div>
               </div>
+
+              {/* bottom description */}
+              <Descriptions weather={weather} units={units} />
             </div>
-  
-            {/* bottom description */}
-            <Descriptions weather={weather} units={units} />
-          </div>
           )
         }
       </div>
